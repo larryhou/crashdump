34d69a5aba3b1218cac272ab9c4262485c8658d3
larryhou <larryhou@tencent.com>
2018-12-26 17:09:51 +0800

    Update

--- crashdump.py
+++ crashdump.py
@@ -34,39 +34,40 @@ def main():
     import argparse, sys
     arguments = argparse.ArgumentParser()
     arguments.add_argument('--symbol-path', '-p', default=p.expanduser('~/Documents/Symbols/Android/godgame'))
-    arguments.add_argument('--crash-file', '-f', required=True)
+    arguments.add_argument('--crash-file', '-f', nargs='+', required=True)
     options = arguments.parse_args(sys.argv[1:])
     for file_name in os.listdir(options.symbol_path):
         if not file_name.endswith('.so'): continue
         libmap[file_name] = p.abspath(p.join(options.symbol_path, file_name))
-    cursor, stack = '', []
-    with open(options.crash_file, 'r+') as fp:
-        output = open(p.dirname(fp.name) + '/dump_' + p.basename(fp.name), 'w+')
-        pattern = re.compile(r'\s+pc\s+([a-f0-9]{8})\s+', re.IGNORECASE)
-        while True:
-            line = fp.readline()
-            if not line:
-                if stack: symbolicate(cursor, stack, output)
-                break
-            match = pattern.search(line)
-            libpos = line.find('/lib/arm/')
-            if not match or libpos == -1:
-                if stack:
-                    symbolicate(cursor, stack, output)
-                    cursor, stack = '', []
-                output.write(line)
-            else:
-                address = pattern.search(line).group(1)
-                libname = p.basename(line[libpos:].split(' ', 1)[0])
-                if not cursor:
-                    cursor, stack = libname, []
-                elif cursor != libname:
-                    symbolicate(cursor, stack, output)
-                    cursor, stack = libname, []
-                stack.append((address, line))
-        output.seek(0)
-        print(output.read())
-        output.close()
+    for crash_file in options.crash_file:
+        cursor, stack = '', []
+        with open(crash_file, 'r+') as fp:
+            output = open(p.dirname(fp.name) + '/dump_' + p.basename(fp.name), 'w+')
+            pattern = re.compile(r'\s+pc\s+([a-f0-9]{8})\s+', re.IGNORECASE)
+            while True:
+                line = fp.readline()
+                if not line:
+                    if stack: symbolicate(cursor, stack, output)
+                    break
+                match = pattern.search(line)
+                libpos = line.find('/lib/arm/')
+                if not match or libpos == -1:
+                    if stack:
+                        symbolicate(cursor, stack, output)
+                        cursor, stack = '', []
+                    output.write(line)
+                else:
+                    address = pattern.search(line).group(1)
+                    libname = p.basename(line[libpos:].split(' ', 1)[0])
+                    if not cursor:
+                        cursor, stack = libname, []
+                    elif cursor != libname:
+                        symbolicate(cursor, stack, output)
+                        cursor, stack = libname, []
+                    stack.append((address, line))
+            output.seek(0)
+            print(output.read())
+            output.close()
 
 
 if __name__ == '__main__':
